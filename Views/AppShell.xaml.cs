ca92fb3455888d0af2d2b7a1886c1e6238e9a00e
Jean.Araujo <jean.rodrigues@sepog.fortaleza.ce.gov.br>
2024-09-11 21:18:10 -0300

    telas de cinemas concluidas

--- Views/AppShell.xaml.cs
+++ Views/AppShell.xaml.cs
@@ -7,6 +7,8 @@
             InitializeComponent();
 
             Routing.RegisterRoute("stores/detail", typeof(Views.Stores.DetailPage));
+            Routing.RegisterRoute("cinemas/detail", typeof(Views.Cinemas.DetailPage));
+            Routing.RegisterRoute("cinemas/detaildesktop", typeof(Views.Cinemas.DetailPageDesktop));
         }
     }
 }
